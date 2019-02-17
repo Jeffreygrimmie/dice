d048f3e92c6008ad9ac1a5e87d4e313c6935220b
Jeffreygrimmie <Jeffreygrimmie@gmail.com>
2019-02-17 11:57:15 -0800

    roshambo.py
    add information display of rolls

--- roshambo.py
+++ roshambo.py
@@ -1,6 +1,5 @@
 ###Jeffrey Grimmie 2/17/2019
 
-
 ###To do: fix bug, if more then one player rolls the same number and the rolls are highest in rolls.
 
 ### tap roll the dice game rolls dice on each input for player then returns what player wins.
@@ -10,28 +9,16 @@ import roll_dice
 def clear_screen():
 	os.system('cls' if os.name == 'nt' else 'clear')
 def same_number_roll_checker(rolls):
-	
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	none = none #place holder for future code
+###To do: fix bug, if more then one player rolls the same number and the rolls are highest in rolls.
+###here
 def roshambo():
 	roll = []
 	rolls = []
 	player_position = 1
 	clear_screen()
 	print("Welcome to Roshambo play at your own risk!!!")
+	print("The player that rolls the highest number first wins!")
 	players = int(input('How many players are there: '))
 	for i in range(players):
 		input('Press any key to roll player %s' % player_position)
@@ -41,9 +28,7 @@ def roshambo():
 		roll[:] = []
 		player_position += 1
 	winner = rolls.index(max(rolls)) + 1
-
-	print(rolls)
-	print('player %s has won the roll!' % winner)
-
+	#print(rolls)
+	print('Player %s has won the roll!' % winner)
 
 roshambo()
